3f8a0d4ff2f60312a98ff44d1c6c5385d0221540
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-12-07 16:22:56 -0800

    wrote frontend, fixed bugs and tested login

---
+++ public/javascript/login.js
@@ -0,0 +1,58 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+    
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+
+
+    if (username &&  password) {
+        const response = await fetch('/api/users', {
+          method: 'post',
+          body: JSON.stringify({
+            username,
+            password
+          }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+        // console.log(response);
+    
+              // check the response status
+              if (response.ok) {
+                // document.location.replace('/');
+                document.location.replace('/dashboard/');
+              } else {
+                alert(response.statusText);
+              }
+      }
+}
+  
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+}
+  
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
